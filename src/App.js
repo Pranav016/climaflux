9a8904f57ed29e899c77351d5a2f03b5eb06598f
Pranav <pranavmendi@gmail.com>
2021-07-19 11:02:22 +0530

    Add css styling and date builder function

--- src/App.js
+++ src/App.js
@@ -1,5 +1,38 @@
 require('dotenv').config();
 
+const dateBuilder = (d) => {
+	let months = [
+		'January',
+		'February',
+		'March',
+		'April',
+		'May',
+		'June',
+		'July',
+		'August',
+		'September',
+		'October',
+		'November',
+		'December',
+	];
+	let days = [
+		'Sunday',
+		'Monday',
+		'Tuesday',
+		'Wednesday',
+		'Thursday',
+		'Friday',
+		'Saturday',
+	];
+
+	let day = days[d.getDay()];
+	let date = d.getDate();
+	let month = months[d.getMonth()];
+	let year = d.getFullYear();
+
+	return `${day} ${date} ${month} ${year}`;
+};
+
 function App() {
 	return (
 		<div className='app'>
@@ -10,6 +43,10 @@ function App() {
 						className='search-bar'
 						placeholder='Enter your location'
 					/>
+				</div>
+				<div className='location-box'>
+					<div className='location'>New York, US</div>
+					<div className='date'>{dateBuilder(new Date())}</div>
 				</div>
 			</main>
 		</div>
